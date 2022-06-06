9ef7b8a747dc344a684aaeab9a2a34418196d2ee
JiniEun <julietjin94@gmail.com>
2022-06-06 14:39:20 +0900

    initialize project

---
+++ src/main/java/com/hong/hongmmunity/HongmmunityApplication.java
@@ -0,0 +1,13 @@
+package com.hong.hongmmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongmmunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongmmunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong/hongmmunity/HongmmunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.hongmmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongmmunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
