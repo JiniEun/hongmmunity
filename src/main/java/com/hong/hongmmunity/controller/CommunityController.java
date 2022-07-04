72160bd63af28c4a9046fd424e7896947d84b524
Jin <julietjin94@gmail.com>
2022-07-04 23:54:19 +0900

    feat : Community delete

--- src/main/java/com/hong/hongmmunity/controller/CommunityController.java
+++ src/main/java/com/hong/hongmmunity/controller/CommunityController.java
@@ -5,7 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,5 +18,19 @@ public class CommunityController {
     private static final Logger log = LoggerFactory.getLogger(CommunityController.class);
 
     private final CommunityService communityService;
+
+
+    @PostMapping("/delete")
+    public String delete(Long communityId) {
+
+        boolean deleteCheck = communityService.deleteById(communityId);
+
+        if (deleteCheck) {
+            return "redirect:/community/list";
+        } else {
+            return "error";
+        }
+
+    }
 
 }
--- src/main/java/com/hong/hongmmunity/service/CommunityService.java
+++ src/main/java/com/hong/hongmmunity/service/CommunityService.java
@@ -1,7 +1,6 @@
 package com.hong.hongmmunity.service;
 
 import com.hong.hongmmunity.dto.CommunityResponseDto;
-import com.hong.hongmmunity.entity.Community;
 import com.hong.hongmmunity.entity.User;
 import com.hong.hongmmunity.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +12,20 @@ public class CommunityService {
     private final CommunityRepository communityRepository;
 
 
-//    public CommunityResponseDto findCommunityByUser(Community community){
-//        return new CommunityResponseDto(communityRepository.findCommunityByUser(community));
-//    }
+    public CommunityResponseDto findCommunityByUser(User user){
+        return new CommunityResponseDto(communityRepository.findCommunityByUser(user));
+    }
+
+    public boolean deleteById(Long communityId){
+        boolean deleteCheck = false;
+        try {
+            communityRepository.deleteById(communityId);
+            deleteCheck = true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return deleteCheck;
+
+    }
 
 }
